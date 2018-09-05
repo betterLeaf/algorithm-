454202dafa2c4087843daed8dda40e4f129480bc
betterLeaf <18623742058@163.com>
2018-09-05 11:12:12 +0800

    add 冒泡

---
+++ js/mp.js
@@ -0,0 +1,20 @@
+/*
+* @Author: betterLeaf
+* @Date:   2018-09-05 11:09:11
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-09-05 11:11:48
+*/
+function mp(arr) {
+	for(let i = 0;i<arr.length;i++){
+	  for(let j = 0; j<arr.length;j++){
+	    if(arr[i]<arr[j]){
+	      let a ='';
+	      a = arr[i];
+	      arr[i] = arr[j];
+	      arr[j] = a;
+	      a='';
+	    }
+	  }
+	}
+	return arr;
+}
